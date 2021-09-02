cowboyd/effection.js

label @effection/process Operations

packages/process/src/exec/win32.ts
@@ -1,5 +1,5 @@
 import { platform } from "os";
-import { spawn, Task, Operation, createFuture } from "@effection/core";
+import { spawn, Task, Operation, createFuture, label, withLabels } from '@effection/core';
 import { createChannel } from '@effection/channel';
 import { on, once, onceEmit } from "@effection/events";
 import { spawn as spawnProcess } from "cross-spawn";
@@ -66,17 +66,26 @@ export const createWin32Process: CreateOSProcess = (command, options) => {
       };
 
       yield spawn(function*(task) {
+        yield label({ name: 'exec', state: 'running' });
         yield spawn(function*() {
-          let value: Error = yield once(childProcess, 'error');
+          yield label({ name: 'listen for error' });
+          let value: Error = yield withLabels(once(childProcess, 'error'), {
+            name: 'untilFirst(error)',
+            source: 'ChildProcess',
+          });
           resolve({ state: 'completed', value: { type: 'error', value } });
         }).within(task);
 
+        // future TODO: label streams for stdout and stderr
         yield spawn(on<Buffer>(childProcess.stdout, 'data').map((c) => c.toString()).forEach(stdoutChannel.send)).within(task);
         yield spawn(on<Buffer>(childProcess.stderr, 'data').map((c) => c.toString()).forEach(stderrChannel.send)).within(task);
 
         try {
-          let value = yield onceEmit(childProcess, "exit");
-          resolve({ state: 'completed', value: { type: "status", value } });
+          let value = yield withLabels(onceEmit(childProcess, 'exit'), {
+            name: 'on(exit)',
+            source: 'ChildProcess',
+          });
+          resolve({ state: 'completed', value: { type: 'status', value } });
         } finally {
           stdoutChannel.close();
           stderrChannel.close();

thefrontside/effectionx

src/exec/win32.ts
@@ -1,5 +1,5 @@
 import { platform } from "os";
-import { spawn, Task, Operation, createFuture } from "@effection/core";
+import { spawn, Task, Operation, createFuture, label, withLabels } from '@effection/core';
 import { createChannel } from '@effection/channel';
 import { on, once, onceEmit } from "@effection/events";
 import { spawn as spawnProcess } from "cross-spawn";
@@ -65,18 +65,26 @@ export const createWin32Process: CreateOSProcess = (command, options) => {
         }
       };
 
-      yield spawn(function*(task) {
-        yield task.spawn(function*() {
-          let value: Error = yield once(childProcess, 'error');
+      yield spawn(function*() {
+        yield spawn(function*() {
+          yield label({ name: 'listen for error' });
+          let value: Error = yield withLabels(once(childProcess, 'error'), {
+            name: 'untilFirst(error)',
+            source: 'ChildProcess',
+          });
           produce({ state: 'completed', value: { type: 'error', value } });
         });
 
-        yield task.spawn(on<Buffer>(childProcess.stdout, 'data').map((c) => c.toString()).forEach(stdoutChannel.send));
-        yield task.spawn(on<Buffer>(childProcess.stderr, 'data').map((c) => c.toString()).forEach(stderrChannel.send));
+        // future TODO: label streams for stdout and stderr
+        yield spawn(on<Buffer>(childProcess.stdout, 'data').map((c) => c.toString()).forEach(stdoutChannel.send));
+        yield spawn(on<Buffer>(childProcess.stderr, 'data').map((c) => c.toString()).forEach(stderrChannel.send));
 
         try {
-          let value = yield onceEmit(childProcess, "exit");
-          produce({ state: 'completed', value: { type: "status", value } });
+          let value = yield withLabels(onceEmit(childProcess, 'exit'), {
+            name: 'on(exit)',
+            source: 'ChildProcess',
+          });
+          produce({ state: 'completed', value: { type: 'status', value } });
         } finally {
           stdoutChannel.close();
           stderrChannel.close();
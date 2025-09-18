c98704b5b43dd1bf709401b50444a4264ec08e04
Taras Mankovski <taras@frontside.com>
2025-09-18 12:23:52 -0400

    [stream-helpers] switched remaining test to @effectionx/bdd

--- stream-helpers/valve.test.ts
+++ stream-helpers/valve.test.ts
@@ -3,46 +3,46 @@ import { createArraySignal, is } from "@effectionx/signals";
 import { expect } from "@std/expect";
 import { assertSpyCalls, spy } from "@std/testing/mock";
 import { each, sleep, spawn } from "effection";
-import { valve } from "./valve.ts";
 
+import { valve } from "./valve.ts";
 import { useFaucet } from "./test-helpers/faucet.ts";
 
 describe("valve", () => {
   it("closes and opens the valve", function* () {
-      const faucet = yield* useFaucet<number>({ open: true });
+    const faucet = yield* useFaucet<number>({ open: true });
 
-      const close = spy(function* () {
-        faucet.close();
-      });
+    const close = spy(function* () {
+      faucet.close();
+    });
 
-      const open = spy(function* () {
-        faucet.open();
-      });
+    const open = spy(function* () {
+      faucet.open();
+    });
 
-      const stream = valve({
-        closeAt: 5,
-        close,
-        open,
-        openAt: 2,
-      });
+    const stream = valve({
+      closeAt: 5,
+      close,
+      open,
+      openAt: 2,
+    });
 
-      const values = yield* createArraySignal<number>([]);
+    const values = yield* createArraySignal<number>([]);
 
-      yield* spawn(function* () {
-        for (const value of yield* each(stream(faucet))) {
-          values.push(value);
-          yield* sleep(1);
-          yield* each.next();
-        }
-      });
+    yield* spawn(function* () {
+      for (const value of yield* each(stream(faucet))) {
+        values.push(value);
+        yield* sleep(1);
+        yield* each.next();
+      }
+    });
 
-      yield* faucet.pour([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+    yield* faucet.pour([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 
-      yield* is(values, (values) => values.length === 10);
+    yield* is(values, (values) => values.length === 10);
 
-      expect(values.valueOf()).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+    expect(values.valueOf()).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 
-      assertSpyCalls(close, 1);
-      assertSpyCalls(open, 1);
+    assertSpyCalls(close, 1);
+    assertSpyCalls(open, 1);
   });
 });

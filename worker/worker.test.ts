810f489b88ec6876188c9246cf8e4c9d3c80a1ad
Taras Mankovski <taras@frontside.com>
2025-09-18 14:31:22 +0200

    Downgraded worker to v3 but with failure

--- worker/worker.test.ts
+++ worker/worker.test.ts
@@ -1,69 +1,77 @@
-import { describe, it } from "@std/testing/bdd";
+import { beforeEach, describe, it } from "@effectionx/bdd";
+import { timebox } from "@effectionx/timebox";
 import { expect } from "@std/expect";
 import { emptyDir, exists } from "@std/fs";
 import { join } from "@std/path";
-import { timebox } from "@effectionx/timebox";
-import { call, run, scoped, sleep, spawn, suspend } from "effection";
+import { call, scoped, sleep, spawn, suspend, until } from "effection";
 
-import { useWorker } from "./worker.ts";
 import { assert } from "@std/assert";
 import type { ShutdownWorkerParams } from "./test-assets/shutdown-worker.ts";
+import { useWorker } from "./worker.ts";
 
 describe("worker", () => {
-  it("sends and receive messages in synchrony", async () => {
-    await run(function* () {
-      let worker = yield* useWorker(
-        import.meta.resolve("./test-assets/echo-worker.ts"),
-        { type: "module" },
-      );
-
-      let result = yield* worker.send("hello world");
-      expect(result).toEqual("hello world");
-    });
-  });
-  it("will raise an exception if an exception happens on the remote side", async () => {
+  it("sends and receive messages in synchrony", function* () {
     expect.assertions(1);
-    let task = run(function* () {
-      let worker = yield* useWorker<void, unknown, unknown, unknown>(
-        import.meta.resolve("./test-assets/boom-worker.ts"),
-        { type: "module" },
-      );
+    let worker = yield* useWorker(
+      import.meta.resolve("./test-assets/echo-worker.ts"),
+      { type: "module" },
+    );
 
+    let result = yield* worker.send("hello world");
+    expect(result).toEqual("hello world");
+  });
+  it("will raise an exception if an exception happens on the remote side", function* () {
+    expect.assertions(2);
+    let worker = yield* useWorker<void, unknown, unknown, unknown>(
+      import.meta.resolve("./test-assets/boom-worker.ts"),
+      { type: "module" },
+    );
+
+    try {
       yield* worker.send();
-    });
-    await expect(task).rejects.toMatchObject({ message: "boom!" });
+    } catch (e) {
+      expect(e).toBeInstanceOf(Error);
+      expect(e).toMatchObject({ message: "boom!" });
+    }
   });
-  it("produces its return value", async () => {
-    await run(function* () {
-      let worker = yield* useWorker(
-        import.meta.resolve("./test-assets/result-worker.ts"),
-        { type: "module", data: "this is the worker result" },
-      );
+  it("produces its return value", function* () {
+    expect.assertions(2);
+    let worker = yield* useWorker(
+      import.meta.resolve("./test-assets/result-worker.ts"),
+      { type: "module", data: "this is the worker result" },
+    );
 
-      expect(yield* worker).toEqual("this is the worker result");
-      expect(yield* worker).toEqual("this is the worker result");
-    });
+    expect(yield* worker).toEqual("this is the worker result");
+    expect(yield* worker).toEqual("this is the worker result");
   });
-  it("raises an exception if the worker raises one", async () => {
-    expect.assertions(1);
-    let task = run(function* () {
-      let worker = yield* useWorker(
-        import.meta.resolve("./test-assets/boom-result-worker.ts"),
-        { type: "module", data: "boom!" },
-      );
+  it("raises an exception if the worker raises one", function* () {
+    expect.assertions(2);
+    let worker = yield* useWorker(
+      import.meta.resolve("./test-assets/boom-result-worker.ts"),
+      { type: "module", data: "boom!" },
+    );
 
-      let result = yield* worker;
-      expect(result).toEqual("hello world");
-    });
-    await expect(task).rejects.toMatchObject({ message: "boom!" });
+    try {
+      yield* worker;
+    } catch (e) {
+      expect(e).toBeInstanceOf(Error);
+      expect(e).toMatchObject({ message: "boom!" });
+    }
   });
-  it("shuts down gracefully", async () => {
-    let dir = new URL(import.meta.resolve("./test-tmp")).pathname;
-    await emptyDir(dir);
-    let startFile = join(dir, "started.txt");
-    let endFile = join(dir, "ended.txt");
-    let url = import.meta.resolve("./test-assets/shutdown-worker.ts");
-    await run(function* () {
+  describe("shutdown", () => {
+    let startFile: string;
+    let endFile: string;
+    let url: string;
+
+    beforeEach(function* () {
+      let dir = new URL(import.meta.resolve("./test-tmp")).pathname;
+      yield* until(emptyDir(dir));
+      startFile = join(dir, "started.txt");
+      endFile = join(dir, "ended.txt");
+      url = import.meta.resolve("./test-assets/shutdown-worker.ts");
+    });
+
+    it("shuts down gracefully", function* () {
       let task = yield* spawn(function* () {
         yield* useWorker(url, {
           type: "module",
@@ -79,7 +87,7 @@ describe("worker", () => {
       let started = yield* timebox(10_000, function* () {
         while (true) {
           yield* sleep(1);
-          if (yield* call(() => exists(startFile))) {
+          if (yield* until(exists(startFile))) {
             break;
           }
         }
@@ -87,59 +95,70 @@ describe("worker", () => {
 
       assert(!started.timeout, "worker did not start after 10s");
       yield* task.halt();
-    });
 
-    expect(await exists(endFile)).toEqual(true);
-    expect(await Deno.readTextFile(endFile)).toEqual("goodbye cruel world!");
+      expect(yield* until(exists(endFile))).toEqual(true);
+      expect(yield* until(Deno.readTextFile(endFile))).toEqual(
+        "goodbye cruel world!",
+      );
+    });
   });
 
-  it("becomes halted if you try and await its value out of scope", async () => {
-    let url = import.meta.resolve("./test-assets/suspend-worker.ts");
-    let task = run(function* () {
+  it(
+    "becomes halted if you try and await its value out of scope",
+    function* () {
+      let url = import.meta.resolve("./test-assets/suspend-worker.ts");
       let worker = yield* scoped(function* () {
         return yield* useWorker(url, { type: "module" });
       });
-      yield* worker;
-    });
-    await expect(task).rejects.toMatchObject({ message: "worker terminated" });
-  });
+      try {
+        yield* worker;
+      } catch (e) {
+        expect(e).toBeInstanceOf(Error);
+        expect(e).toMatchObject({ message: "worker terminated" });
+      }
+    },
+  );
 
-  it("supports stateful operations", async () => {
-    let url = import.meta.resolve("./test-assets/counter-worker.ts");
+  it("supports stateful operations", function* () {
     expect.assertions(3);
 
-    await run(function* () {
-      let worker = yield* useWorker(url, { type: "module", data: 2 });
+    let url = import.meta.resolve("./test-assets/counter-worker.ts");
 
-      expect(yield* worker.send(10)).toEqual(12);
+    let worker = yield* useWorker(url, { type: "module", data: 2 });
 
-      expect(yield* worker.send(-5)).toEqual(7);
+    expect(yield* worker.send(10)).toEqual(12);
 
-      expect(yield* worker.send(35)).toEqual(42);
-    });
+    expect(yield* worker.send(-5)).toEqual(7);
+
+    expect(yield* worker.send(35)).toEqual(42);
   });
 
-  it.skip("crashes if there is an uncaught error in the worker", async () => {
+  it.skip("crashes if there is an uncaught error in the worker", function* () {
     let crash = import.meta.resolve("./test-assets/crash-worker.ts");
-    let task = run(function* () {
-      let worker = yield* useWorker(crash, { name: "crash", type: "module" });
+    let worker = yield* useWorker(crash, { name: "crash", type: "module" });
+    try {
       yield* worker;
-    });
-    await expect(task).rejects.toMatchObject({ message: "boom!" });
+    } catch (e) {
+      expect(e).toBeInstanceOf(Error);
+      expect(e).toMatchObject({ message: "boom!" });
+    }
   });
-  it.skip("crashes if the worker module cannot be found", async () => {
+
+  it.skip("crashes if the worker module cannot be found", function* () {
     let crash = import.meta.resolve("./test-assets/non-existent-worker.ts");
-    let task = run(function* () {
-      let worker = yield* useWorker(crash, { name: "crash", type: "module" });
+    let worker = yield* useWorker(crash, { name: "crash", type: "module" });
+    try {
       yield* worker;
-    });
-    await expect(task).rejects.toMatchObject({ message: "boom!" });
+    } catch (e) {
+      expect(e).toBeInstanceOf(Error);
+      expect(e).toMatchObject({ message: "boom!" });
+    }
   });
-  it.skip("crashes if there is a message error from the main thread", async () => {
+  it.skip("crashes if there is a message error from the main thread", function* () {
     // don't know how to reproduce this
   });
 
-  it.skip("crashes if there is a message error from the worker thread", async () => {
+  it.skip("crashes if there is a message error from the worker thread", function* () {
     // don't know how to trigger
   });
 });

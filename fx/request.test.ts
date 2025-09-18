e802705564ad85332c2e4abbcaba555fb0aacc96
Taras Mankovski <taras@frontside.com>
2025-09-18 11:47:58 -0400

    [fx] removed dependency on external service in tests

--- fx/request.test.ts
+++ fx/request.test.ts
@@ -1,26 +1,31 @@
-import { describe, it } from "@effectionx/bdd";
+import { beforeEach, describe, it } from "@effectionx/bdd";
 import { expect } from "@std/expect";
 
 import { json, request } from "./request.ts";
+import { ensure, until } from "effection";
 
 // Ensure to run tests with --allow-net permission
 describe("request() and json()", () => {
-  it("should fetch a URL and return a response", function* () {
-    const response = yield* request(
-      "https://jsonplaceholder.typicode.com/todos/1",
+  let url: string;
+  beforeEach(function* () {
+    let server = Deno.serve(
+      () =>
+        new Response(JSON.stringify({ id: 1, title: "do things" }), {
+          headers: new Headers({
+            "Content-Type": "application/json",
+          }),
+        }),
     );
-    expect(response.ok).toBe(true);
-    expect(response.status).toBe(200);
+
+    url = `http://${server.addr.hostname}:${server.addr.port}/todos/1`,
+      yield* ensure(() => until(server.shutdown()));
   });
 
-  it("should parse JSON from a response", function* () {
-    const response = yield* request(
-      "https://jsonplaceholder.typicode.com/todos/1",
-    );
+  it("returns a response that can be parsed with json", function* () {
+    const response = yield* request(url);
     const data = yield* json(response);
-  
+
     expect(data).toHaveProperty("id", 1);
     expect(data).toHaveProperty("title");
   });
 });
-

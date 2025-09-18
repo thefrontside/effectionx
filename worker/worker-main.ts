9149bcc13c2f7ab8206d051a439857d6ef475558
Taras Mankovski <taras@frontside.com>
2025-09-18 10:42:16 -0400

    All worker tests are passing

--- worker/worker-main.ts
+++ worker/worker-main.ts
@@ -1,3 +1,4 @@
+import type { ValueSignal } from "@effectionx/signals";
 import {
   createSignal,
   each,
@@ -6,11 +7,11 @@ import {
   Ok,
   on,
   type Operation,
-  type Result,
-  scoped,
+  resource,
   spawn,
-  withResolvers,
+  type Task,
 } from "effection";
+
 import type { WorkerControl, WorkerMainOptions } from "./types.ts";
 
 /**
@@ -63,61 +64,163 @@ import type { WorkerControl, WorkerMainOptions } from "./types.ts";
  * @param {(options: WorkerMainOptions<TSend, TRecv, TData>) => Operation<TReturn>} body
  * @returns {Promise<void>}
  */
-
 export async function workerMain<TSend, TRecv, TReturn, TData>(
   body: (options: WorkerMainOptions<TSend, TRecv, TData>) => Operation<TReturn>,
 ): Promise<void> {
   await main(function* () {
     let sent = createSignal<{ value: TSend; response: MessagePort }>();
-    let controls = yield* on(self, "message");
-    let outcome = withResolvers<Result<TReturn>>();
+    let worker = yield* createWorkerStatesSignal();
 
-    self.postMessage({ type: "open" });
-
-    let result = yield* scoped(function* () {
-      yield* spawn(function* () {
-        let next = yield* controls.next();
-        while (true) {
-          let control: WorkerControl<TSend, TData> = next.value.data;
-          if (control.type === "init") {
-            yield* spawn(function* () {
-              try {
-                let value = yield* body({
-                  data: control.data,
-                  messages: {
-                    *forEach(fn: (value: TSend) => Operation<TRecv>) {
-                      for (let { value, response } of yield* each(sent)) {
-                        yield* spawn(function* () {
-                          try {
-                            let result = yield* fn(value);
-                            response.postMessage(Ok(result));
-                          } catch (error) {
-                            response.postMessage(Err(error as Error));
-                          }
-                        });
-                        yield* each.next();
-                      }
+    yield* spawn(function* () {
+      for (const message of yield* each(on(self, "message"))) {
+        const control: WorkerControl<TSend, TData> = message.data;
+        switch (control.type) {
+          case "init": {
+            worker.start(
+              yield* spawn(function* () {
+                try {
+                  let value = yield* body({
+                    data: control.data,
+                    messages: {
+                      *forEach(fn: (value: TSend) => Operation<TRecv>) {
+                        for (let { value, response } of yield* each(sent)) {
+                          yield* spawn(function* () {
+                            try {
+                              let result = yield* fn(value);
+                              response.postMessage(Ok(result));
+                            } catch (error) {
+                              response.postMessage(Err(error as Error));
+                            }
+                          });
+                          yield* each.next();
+                        }
+                      },
                     },
-                  },
-                });
+                  });
 
-                outcome.resolve(Ok(value));
-              } catch (error) {
-                outcome.resolve(Err(error as Error));
-              }
-            });
-          } else if (control.type === "send") {
+                  worker.complete(value);
+                } catch (error) {
+                  worker.crash(error as Error);
+                }
+              }),
+            );
+            break;
+          }
+          case "send": {
             let { value, response } = control;
             sent.send({ value, response });
-          } else if (control.type === "close") {
-            outcome.resolve(Err(new Error(`worker terminated`)));
+            break;
           }
-          next = yield* controls.next();
+          case "close": {
+            const current = worker.valueOf();
+            if (current.type === "running") {
+              yield* current.task.halt();
+              worker.interrupt();
+            }
+          }
         }
-      });
 
-      return yield* outcome.operation;
+        yield* each.next();
+      }
     });
-    self.postMessage({ type: "close", result });
+
+    for (const state of yield* each(worker)) {
+      if (state.type === "new") {
+        postMessage({ type: "open" });
+      } else if (state.type === "interrupted" || state.type === "error") {
+        postMessage({ type: "close", result: Err(state.error) });
+      } else if (state.type === "complete") {
+        postMessage({ type: "close", result: Ok(state.value) });
+      }
+      yield* each.next();
+    }
   });
+}
+
+type New = { type: "new" };
+type Running = { type: "running"; task: Task<void> };
+type Complete = { type: "complete"; value: unknown };
+type Errored = { type: "error"; error: Error };
+type Interrupted = { type: "interrupted"; error: Error };
+
+type WorkerState = New | Running | Complete | Errored | Interrupted;
+
+interface WorkerStateSignal extends ValueSignal<WorkerState> {
+  state: WorkerState["type"];
+  start(task: Task<void>): Running | Complete;
+  complete(value: unknown): Complete;
+  crash(error: Error): Errored;
+  interrupt(): Interrupted;
+}
+
+export function createWorkerStatesSignal(): Operation<WorkerStateSignal> {
+  return resource(function* (provide) {
+    let ref: { current: WorkerState } = {
+      current: { type: "new" },
+    };
+    const signal = createSignal<WorkerState>();
+
+    const set: WorkerStateSignal["set"] = (value) => {
+      signal.send(ref.current = value);
+      return value;
+    };
+
+    const update: WorkerStateSignal["update"] = (updater) =>
+      set(updater(ref.current));
+
+    const interrupt: WorkerStateSignal["interrupt"] = () => {
+      let next: Interrupted = {
+        type: "interrupted",
+        error: new Error(`worker terminated`),
+      };
+      set(next);
+      return next;
+    };
+
+    try {
+      yield* provide({
+        *[Symbol.iterator]() {
+          let subscription = yield* signal;
+          signal.send(ref.current);
+          return subscription;
+        },
+        get state() {
+          return ref.current.type;
+        },
+        set,
+        update,
+        valueOf() {
+          return ref.current;
+        },
+        start(task) {
+          if (ref.current.type === "complete") {
+            return ref.current;
+          } else {
+            const next: Running = { type: "running", task };
+            set(next);
+            return next;
+          }
+        },
+        complete(value) {
+          let next: Complete = { type: "complete", value };
+          set(next);
+          return next;
+        },
+        crash(error) {
+          let next: Errored = { type: "error", error };
+          set(next);
+          return next;
+        },
+        interrupt: interrupt,
+      });
+    } finally {
+      if (ref.current.type === "running") {
+        interrupt();
+      }
+    }
+  });
+}
+
+function postMessage(message: unknown): void {
+  self.postMessage(message);
 }

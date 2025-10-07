6ee8fb0ffefa784ea036ae65129f8b30715a5c5a
Taras Mankovski <tarasm@gmail.com>
2025-10-06 21:18:34 -0700

    Removed unecessary graceful shutdown test because it's covered in other tests

--- process/test/helpers.ts
+++ process/test/helpers.ts
@@ -1,32 +1,5 @@
-import { ctrlc } from "ctrlc-windows";
-import type { Process } from "../src/exec.ts";
 import { type Operation, type Stream, until } from "effection";
 import { filter } from "@effectionx/stream-helpers";
-
-const isWin32 = globalThis.process.platform === "win32";
-
-export function terminate(process: Process): void {
-  if (isWin32) {
-    ctrlc(process.pid);
-    //Terminate batch process? (Y/N)
-    process.stdin.send("Y\n");
-  } else {
-    globalThis.process.kill(-process.pid, "SIGTERM");
-  }
-}
-
-// cross platform user initiated graceful shutdown request. What would
-// be sent to the process by the Operating system when
-// a users requests an interrupt via CTRL-C or equivalent.
-export function interrupt(process: Process): void {
-  if (isWin32) {
-    ctrlc(process.pid);
-    //Terminate batch process? (Y/N)
-    process.stdin.send("Y\n");
-  } else {
-    globalThis.process.kill(-process.pid, "SIGINT");
-  }
-}
 
 export function* captureError(op: Operation<unknown>): Operation<Error> {
   try {

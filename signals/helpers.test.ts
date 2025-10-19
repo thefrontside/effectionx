f6a8194acbb277d3c876477c6f2d440226f329c6
Taras Mankovski <taras@frontside.com>
2025-10-18 20:32:28 -0400

    Fixed is helper test by giving it 1 tick to setup the operation tree

--- signals/helpers.test.ts
+++ signals/helpers.test.ts
@@ -1,6 +1,6 @@
 import { describe, it } from "@effectionx/bdd";
 import { expect } from "@std/expect";
-import { spawn } from "effection";
+import { sleep, spawn, withResolvers } from "effection";
 
 import { createBooleanSignal } from "./boolean.ts";
 import { is } from "./helpers.ts";
@@ -9,13 +9,21 @@ describe("is", () => {
   it("waits until the value of the stream matches the predicate", function* () {
     const open = yield* createBooleanSignal(false);
     const update: string[] = [];
+    
+    const { resolve, operation } = withResolvers<void>();
 
     yield* spawn(function* () {
       yield* is(open, (open) => open === true);
       update.push("floodgates are open!");
+      resolve();
     });
 
-    open.set(true);
+    yield* spawn(function* () {
+      yield* sleep(1);
+      open.set(true);
+    });
+
+    yield* operation;
 
     expect(update).toEqual(["floodgates are open!"]);
   });

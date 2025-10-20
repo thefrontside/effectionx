72ed7496a8e3a89037e4064b51566a6d784ba721
Charles Lowell <cowboyd@frontside.com>
2025-10-20 13:45:32 -0500

    Make the sleeps for v3 compat, not v4 compat

--- stream-helpers/for-each.test.ts
+++ stream-helpers/for-each.test.ts
@@ -1,4 +1,4 @@
-import { createChannel, sleep, spawn, withResolvers } from "effection";
+import { createChannel, sleep, spawn } from "effection";
 import { describe, it } from "@effectionx/bdd";
 import { expect } from "@std/expect";
 
@@ -11,44 +11,32 @@ describe("forEach", () => {
     const stream = createChannel<number, void>();
     const processedItems: number[] = [];
 
-    const { resolve, operation } = withResolvers<void>();
-
-    yield* spawn(() =>
-      forEach(function* (item: number) {
-        processedItems.push(item);
-      }, stream)
-    );
-
     yield* spawn(function* () {
-      yield* sleep(1);
+      yield* sleep(0);
       yield* stream.send(1);
       yield* stream.send(2);
       yield* stream.send(3);
-
-      resolve();
+      yield* stream.close();
     });
 
-    yield* operation;
+    yield* forEach(function* (item: number) {
+      processedItems.push(item);
+    }, stream);
+
     expect(processedItems).toEqual([1, 2, 3]);
   });
 
   it("should return the close value of the stream", function* () {
     const stream = createChannel<string, number>();
 
-    const result = yield* spawn(() =>
-      forEach(function* () {
-        // Just process the item
-      }, stream)
-    );
-
     yield* spawn(function* () {
-      yield* sleep(1);
+      yield* sleep(0);
       yield* stream.send("hello");
       yield* stream.send("world");
       yield* stream.close(42); // Close with value 42
     });
 
-    const closeValue = yield* result;
+    const closeValue = yield* forEach(function* () {}, stream);
     expect(closeValue).toBe(42);
   });
 });

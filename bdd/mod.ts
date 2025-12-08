73cb39bc41896c24412872dd9f54a05c56aa579c
Taras Mankovski <taras@frontside.com>
2025-12-07 19:22:04 -0500

    Add sanitization options to test functions

--- bdd/mod.ts
+++ bdd/mod.ts
@@ -6,16 +6,37 @@ import {
   it as $it,
 } from "@std/testing/bdd";
 
+/**
+ * Sanitization options for test cases and test suites.
+ * These options control Deno's test sanitizers.
+ */
+export interface SanitizeOptions {
+  /** Ensure the test case does not prematurely cause the process to exit. Defaults to true. */
+  sanitizeExit?: boolean;
+  /** Check that the number of async completed ops after the test is the same as number of dispatched ops. Defaults to true. */
+  sanitizeOps?: boolean;
+  /** Ensure the test case does not "leak" resources - ie. the resource table after the test has exactly the same contents as before the test. Defaults to true. */
+  sanitizeResources?: boolean;
+}
+
 let current: TestAdapter | undefined;
 
-export function describe(name: string, body: () => void) {
+export function describe(
+  name: string,
+  body: () => void,
+  options?: SanitizeOptions,
+) {
   const original = current;
   try {
     const child = current = createTestAdapter({ name, parent: original });
 
-    $describe(name, () => {
-      $afterAll(() => child.destroy());
-      body();
+    $describe({
+      name,
+      fn: () => {
+        $afterAll(() => child.destroy());
+        body();
+      },
+      ...options,
     });
   } finally {
     current = original;
@@ -23,7 +44,27 @@ export function describe(name: string, body: () => void) {
 }
 
 describe.skip = $describe.skip;
-describe.only = $describe.only;
+describe.only = function (
+  name: string,
+  body: () => void,
+  options?: SanitizeOptions,
+): void {
+  const original = current;
+  try {
+    const child = current = createTestAdapter({ name, parent: original });
+
+    $describe.only({
+      name,
+      fn: () => {
+        $afterAll(() => child.destroy());
+        body();
+      },
+      ...options,
+    });
+  } finally {
+    current = original;
+  }
+};
 
 export function beforeAll(body: () => Operation<void>) {
   current?.addOnetimeSetup(body);
@@ -33,17 +74,25 @@ export function beforeEach(body: () => Operation<void>) {
   current?.addSetup(body);
 }
 
-export function it(desc: string, body?: () => Operation<void>): void {
+export function it(
+  desc: string,
+  body?: () => Operation<void>,
+  options?: SanitizeOptions,
+): void {
   const adapter = current!;
   if (!body) {
     $it.skip(desc, () => {});
     return;
   }
-  $it(desc, async () => {
-    const result = await adapter.runTest(body);
-    if (!result.ok) {
-      throw result.error;
-    }
+  $it({
+    name: desc,
+    fn: async () => {
+      const result = await adapter.runTest(body);
+      if (!result.ok) {
+        throw result.error;
+      }
+    },
+    ...options,
   });
 }
 
@@ -52,12 +101,20 @@ it.skip = (...args: Parameters<typeof it>): ReturnType<typeof it> => {
   return $it.skip(desc, () => {});
 };
 
-it.only = (desc: string, body: () => Operation<void>): void => {
+it.only = (
+  desc: string,
+  body: () => Operation<void>,
+  options?: SanitizeOptions,
+): void => {
   const adapter = current!;
-  $it.only(desc, async () => {
-    const result = await adapter.runTest(body);
-    if (!result.ok) {
-      throw result.error;
-    }
+  $it.only({
+    name: desc,
+    fn: async () => {
+      const result = await adapter.runTest(body);
+      if (!result.ok) {
+        throw result.error;
+      }
+    },
+    ...options,
   });
 };

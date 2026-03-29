22aaedc78425388fd1aeb7a5572a90fb87c1287e
Jacob Bolda <me@jacobbolda.com>
2026-03-28 20:16:20 -0500

    remove context api change

--- context-api/mod.ts
+++ context-api/mod.ts
@@ -60,7 +60,7 @@ type FieldMiddleware = {
 type MiddlewareRegistry<A> = Record<keyof A, FieldMiddleware>;
 
 export function createApi<A extends {}>(name: string, handler: A): Api<A> {
-  let fields = Object.keys(handler as object) as (keyof A)[];
+  let fields = Object.keys(handler) as (keyof A)[];
 
   let initial = fields.reduce(
     (sum, field) => {

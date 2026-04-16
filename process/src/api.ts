5d286cc5c39e3ec5b928a5f66349f67353fa0592
Taras Mankovski <taras@frontside.com>
2026-04-15 20:34:58 -0400

    Rename Stdio api scope to @effectionx/stdio

--- process/src/api.ts
+++ process/src/api.ts
@@ -38,7 +38,7 @@ import type { StdioApi } from "./exec/types.ts";
  * });
  * ```
  */
-export const Stdio = createApi<StdioApi>("@effectionx/process", {
+export const Stdio = createApi<StdioApi>("@effectionx/stdio", {
   *stdout(line) {
     process.stdout.write(line);
   },
